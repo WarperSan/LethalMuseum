62705ba20fe6c00fb161375c93aa05f62f451598
WarperSan <leumas.ecole@gmail.com>
2025-05-13 16:53:05 -0400

    Push

---
+++ LethalMuseum/UI/ItemBoard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LethalMuseum.UI;
+
+#pragma warning disable CS0649
+
+public class ItemBoard : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField] private Image? icon;
+    [SerializeField] private TMP_Text? text;
+    [SerializeField] private List<Sprite>? invalidIcons;
+
+    #endregion
+
+    public void SetItem(Item item)
+    {
+        var isValidIcon = invalidIcons?.Contains(item.itemIcon) ?? true;
+
+        if (icon != null)
+        {
+            icon.sprite = item.itemIcon;
+            icon.enabled = isValidIcon;
+        }
+
+        if (text != null)
+        {
+            text.text = item.itemName ?? "Scrap";
+            text.enabled = !isValidIcon;
+        }
+    }
+}
---
+++ LethalMuseum/UI/ItemsBoard.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LethalMuseum.UI;
+
+#pragma warning disable CS0649
+
+public class ItemsBoard : MonoBehaviour
+{
+    internal static ItemsBoard? Instance;
+    
+    #region Fields
+
+    [Header("Items")]
+    [SerializeField] private Transform? itemsContainer;
+    [SerializeField] private GameObject? itemPrefab;
+
+    [Header("Amounts")]
+    [SerializeField] private TMP_Text? collectedAmountText;
+    [SerializeField] private TMP_Text? totalAmountText;
+    [SerializeField] private TMP_Text? percentAmountText;
+    
+    [Header("Pages")]
+    [SerializeField] private TMP_Text? pageText;
+    [SerializeField] private Graphic? leftPageIcon;
+    [SerializeField] private Graphic? rightPageIcon;
+    
+    #endregion
+
+    #region Unity
+
+    private void Start()
+    {
+        Instance = this;
+    }
+
+    #endregion
+
+    public void SetItems(Item[] items)
+    {
+        if (itemsContainer == null)
+            return;
+
+        foreach (Transform child in itemsContainer)
+            Destroy(child);
+        
+        if (itemPrefab == null)
+            return;
+        
+        foreach (var item in items)
+        {
+            var instance = Instantiate(itemPrefab, itemsContainer, false);
+            
+            if (instance.TryGetComponent(out ItemBoard itemBoard))
+                itemBoard.SetItem(item);
+        }
+    }
+}
