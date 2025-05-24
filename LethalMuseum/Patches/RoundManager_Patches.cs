47fb74dbb7ff9c78534b1eeb8e6fb3576eae63c3
WarperSan <leumas.ecole@gmail.com>
2025-05-23 21:58:19 -0400

    Added patches to git

---
+++ LethalMuseum/Patches/RoundManager_Patches.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using LethalMuseum.Helpers;
+using LethalMuseum.Objects;
+
+namespace LethalMuseum.Patches;
+
+[HarmonyPatch(typeof(RoundManager))]
+internal class RoundManager_Patches
+{
+    [HarmonyPatch(nameof(RoundManager.DespawnPropsAtEndOfRound)), HarmonyPostfix]
+    private static void DespawnPropsAtEndOfRound_Postfix(RoundManager __instance)
+    {
+        Logger.Info("DESPAWN PROPS AT THE END OF ROUND");
+        Logger.Info(Tracker.Instance == null);
+        Tracker.Instance?.ResetCollected();
+    }
+}
---
+++ LethalMuseum/Patches/StartOfRound_Patches.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using LethalMuseum.Helpers;
+using LethalMuseum.Objects;
+
+namespace LethalMuseum.Patches;
+
+[HarmonyPatch(typeof(StartOfRound))]
+internal class StartOfRound_Patches
+{
+    [HarmonyPatch(nameof(StartOfRound.ResetShip)), HarmonyPostfix]
+    private static void ResetShip_Postfix()
+    {
+        Logger.Info("RESET SHIP");
+        Logger.Info(Tracker.Instance == null);
+        Tracker.Instance?.ResetCollected();
+    }
+}
